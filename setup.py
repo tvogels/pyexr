0e8e0e43b848fde2326773ca47ddca68fd9c1b78
Thijs Vogels <t.vogels@me.com>
2016-05-23 17:21:52 +0200

    Change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name             = "PyEXR",
-  version          = "0.2",
+  version          = "0.2.1",
   description      = "One line EXR manipulation library",
   author           = "Thijs Vogels",
   author_email     = "t.vogels@me.com",
