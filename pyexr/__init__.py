ebd4d4f72dc5d8c7890712695561677842a5911b
Thijs Vogels <t.vogels@me.com>
2016-05-25 09:42:40 +0200

    Export compression types

--- pyexr/__init__.py
+++ pyexr/__init__.py
@@ -3,3 +3,4 @@ from exr import read
 from exr import write
 from exr import tonemap
 from exr import UINT, FLOAT, HALF
+from exr import NO_COMPRESSION, RLE_COMPRESSION, ZIPS_COMPRESSION, ZIP_COMPRESSION, PIZ_COMPRESSION, PXR24_COMPRESSION
