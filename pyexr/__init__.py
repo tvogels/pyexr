c140ea5ea1a66d439f8b48ea957efe55726b8375
Thijs Vogels <t.vogels@me.com>
2016-05-30 19:58:04 +0200

    Export read_all and read_dict

--- pyexr/__init__.py
+++ pyexr/__init__.py
@@ -2,5 +2,7 @@ from exr import open
 from exr import read
 from exr import write
 from exr import tonemap
+from exr import read_all
+from exr import read_dict
 from exr import UINT, FLOAT, HALF
 from exr import NO_COMPRESSION, RLE_COMPRESSION, ZIPS_COMPRESSION, ZIP_COMPRESSION, PIZ_COMPRESSION, PXR24_COMPRESSION
