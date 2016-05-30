08f79844199056f491ca01614a2c4288ee052471
Thijs Vogels <t.vogels@me.com>
2016-05-30 19:58:59 +0200

    Shouldn't have exported unexisting read_dict

--- pyexr/__init__.py
+++ pyexr/__init__.py
@@ -3,6 +3,5 @@ from exr import read
 from exr import write
 from exr import tonemap
 from exr import read_all
-from exr import read_dict
 from exr import UINT, FLOAT, HALF
 from exr import NO_COMPRESSION, RLE_COMPRESSION, ZIPS_COMPRESSION, ZIP_COMPRESSION, PIZ_COMPRESSION, PXR24_COMPRESSION
