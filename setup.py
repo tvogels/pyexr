d0598bbfe07bde29e7d449cd45c40b31d69237f9
Thijs Vogels <t.vogels@me.com>
2016-09-17 16:30:13 +0200

    Change OpenEXR dependency to 1.3

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
   name             = "PyEXR",
-  version          = "0.3.4",
+  version          = "0.3.5",
   description      = "One line EXR manipulation library",
   author           = "Thijs Vogels",
   author_email     = "t.vogels@me.com",
   url              = "https://github.com/tvogels/pyexr",
   install_requires = ["OpenEXR","numpy","future"],
+  dependency_links = ["https://github.com/jamesbowman/openexrpython/tarball/master#egg=OpenEXR-1.3.0"],
   packages         = find_packages(),
 )
