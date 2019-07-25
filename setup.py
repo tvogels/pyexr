04c3a2310789d7c293a0ec5b6dc3a51dd641985f
Thijs Vogels <thijs.vogels@epfl.ch>
2019-07-25 22:29:37 +0200

    Version bump 0.3.6
    This closes #6.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-  name             = "PyEXR",
-  version          = "0.3.5",
-  description      = "One line EXR manipulation library",
-  author           = "Thijs Vogels",
-  author_email     = "t.vogels@me.com",
-  url              = "https://github.com/tvogels/pyexr",
-  install_requires = ["OpenEXR","numpy","future"],
-  dependency_links = ["https://github.com/jamesbowman/openexrpython/tarball/master#egg=OpenEXR-1.3.0"],
-  packages         = find_packages(),
+    name="PyEXR",
+    version="0.3.6",
+    description="One line EXR manipulation library",
+    author="Thijs Vogels",
+    author_email="t.vogels@me.com",
+    url="https://github.com/tvogels/pyexr",
+    install_requires=["OpenEXR", "numpy", "future"],
+    dependency_links=[
+        "https://github.com/jamesbowman/openexrpython/tarball/master#egg=OpenEXR-1.3.0"
+    ],
+    packages=find_packages(),
 )
